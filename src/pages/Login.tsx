5c87c568b2d087c6a0df2538509b25a0f2fb99d9
MAHossain1 <mahpro110@gmail.com>
2024-07-26 13:00:17 +0600

    Implemented redux, persist user and protected routes.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from 'antd';
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
+import { useDispatch } from 'react-redux';
+import { verifyToken } from '../utils/verifyToken';
+import { setUser } from '../redux/features/auth/authSlice';
+
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: 'A-0002',
+      password: 'ami12345',
+    },
+  });
+
+  const [login] = useLoginMutation();
+
+  // console.log('data=>', data);
+  // console.log('error=>', error);
+
+  const onSubmit = async (data: any) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
   return (
-    <div>
-      <h1>Login...</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>{' '}
+        <input type="text" id="id" {...register('userId')} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" {...register('password')} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
