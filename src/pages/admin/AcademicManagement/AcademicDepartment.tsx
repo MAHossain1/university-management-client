8a647dbb02f31302b3e5143e86f57ca540f548dd
MAHossain1 <mahpro110@gmail.com>
2024-08-19 09:55:25 +0600

    Retrieved and shown all academic departments.

--- src/pages/admin/AcademicManagement/AcademicDepartment.tsx
+++ src/pages/admin/AcademicManagement/AcademicDepartment.tsx
@@ -1,8 +1,55 @@
+import { Button, Table, TableColumnsType } from 'antd';
+import { useGetAcademicDepartmentsQuery } from '../../../redux/features/admin/academicManagement.api';
+import { TAcademicDepartment } from '../../../types/academicManagement.type';
+
+export type TTableData = Pick<TAcademicDepartment, 'name' | 'academicFaculty'>;
+
+type TAcademicFacultyProps = {
+  _id: string;
+  name: string;
+  academicFaculty: any;
+};
+
 const AcademicDepartment = () => {
+  const { data: academicDepartmentData, isFetching } =
+    useGetAcademicDepartmentsQuery();
+
+  const tableData = academicDepartmentData?.data?.map(
+    ({ _id, name, academicFaculty }: TAcademicFacultyProps) => ({
+      key: _id,
+      name,
+      academicFaculty: academicFaculty.name,
+    })
+  );
+
+  console.log(tableData);
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      showSorterTooltip: { target: 'full-header' },
+    },
+    {
+      title: 'Academic Faculty',
+      dataIndex: 'academicFaculty',
+    },
+
+    {
+      title: 'Action',
+      key: 'x',
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>
-      <h1>Academic Department</h1>
-    </div>
+    <Table loading={isFetching} columns={columns} dataSource={tableData} />
   );
 };
 
